fa7badd7e13128e5d1217e2a9c85a053246a4da8
mauricio2002 <mauricio200243@gmail.com>
2019-05-29 14:52:51 +0800

    確認項目表示
    確認画面にnickname,email,contentの値を表示できるように追加。
    また値がない場合はエラーメッセージを表示するように変更。

---
+++ check.php
@@ -0,0 +1,39 @@
+<?php
+    // スーパーグローバル関数
+    $nickname = $_POST['nickname'];
+    $email = $_POST['email'];
+    $content = $_POST['content'];
+    // echo $nickname;
+
+    if ($nickname == '') {
+        $nickname_result = 'ニックネームが入力されていません。';
+    } else {
+        $nickname_result = 'ようこそ、' . $nickname . '様';
+    }
+
+    if ($email == '') {
+        $email_result = 'メールアドレスが入力されていません。';
+    } else {
+        $email_result = 'メールアドレス' . $email;
+    }
+
+    if ($content == '') {
+        $content_result = 'お問い合わせ内容が入力されていません。';
+    } else {
+        $content_result = 'お問い合わせ内容:' . $content;
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>入力内容確認</title>
+</head>
+<body>
+    <h1>入力内容確認</h1>
+    <p><?php echo $nickname_result ?></p>
+    <p><?php echo $email_result ?></p>
+    <p><?php echo $content_result ?></p>
+</body>
+</html>
